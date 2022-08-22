36be59fc09a986f348f324edd7ddd6cd0b8d3fc9
Jepasq <jepasq111@gmail.com>
2022-08-22 15:59:33 +0200

    Move actual source to index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,69 @@
 <?php
-echo "Hello, we're live. If you see this text, <em>php</em> works!";
+include('VPKReader/Exception.php');
+include('VPKReader/VPKHeader.php');
+include('VPKReader/VPKDirectoryEntry.php');
+include('VPKReader/VPKFile.php');
+include('VPKReader/VPK.php');
 
-phpinfo();
+/*
+echo get_include_path();
+echo ini_get('include_path');
+*/
 
+$home = $_SERVER['HOME'];
+$dota = "$home/.steam/steam/steamapps/common/dota 2 beta/game/dota/";
+
+
+$vpk_file = $dota.'pak01_dir.vpk';
+$vpk = new VPKReader\VPK($vpk_file);
+$ent_tree = $vpk->vpk_entries;
+
+/*
+$data = $vpk->read_file($dota.'pak01_000.vpk', 10000);
+echo $data;
+*/
+
+
+// Based on https://stackoverflow.com/a/31685070
+echo '<html><head>';
+echo '<script type="text/javascript">
+$("#chaps > li").click(function() {
+  $(this).find("ul.assignments").toggleClass("hide");
+});
+</script>';
+
+echo '<link href="style.css" rel="stylesheet" type="text/css">
+</head>';
+
+echo '<html><div id="chaps">';
+
+
+$print_tree = function($node, $pwd='') use (&$print_tree){
+    $hide='';
+    if (is_countable($node)) {
+        if(!is_null($node) && count($node) > 0) {
+            if(is_array($node)){
+                echo "<ul class='assignments $hide'>";
+                foreach($node as $name=>$subn) {
+                    $fp = "$pwd/$name";
+                    echo "<li>$fp";
+                    /*                    $hide='hide';
+                    $print_tree($subn, $fp);
+                    $hide='';
+                    */
+                    echo "</li>\n";
+                }
+                echo '</ul>';
+            }else{ // Node
+                echo " | size: $node->size bytes";
+            }
+        } else { // !countable
+            echo "Not countable\n";
+        }
+    }
+};
+$print_tree($ent_tree);
+
+
+echo '</div></html></head>';
 ?>
