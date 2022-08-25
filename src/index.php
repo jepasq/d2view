2e4938f5cafcf816717db9fae52ef3018068955d
Jepasq <jepasq111@gmail.com>
2022-08-25 15:50:13 +0200

    Add an alert if config file not found

--- src/index.php
+++ src/index.php
@@ -1,12 +1,11 @@
 <?php
 
-include('../config.php');
 
 include('D2view.php');
 
 
 function navbar() {
-    echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    echo '<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -14,16 +13,15 @@ function navbar() {
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home
+          <span class="sr-only">(current)</span>
+        </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Features</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
   </div>
@@ -54,12 +52,13 @@ function file_perms($dir) {
     }
 }
 
+
+
 /*
 echo get_include_path();
 echo ini_get('include_path');
 */
 
-$dota = "$home/.steam/steam/steamapps/common/dota 2 beta/game/dota/";
 
 
 /*
@@ -76,9 +75,21 @@ $("#chaps > li").click(function() {
 });
 </script>';
 echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">';
+echo '<link href="style.css?5" rel="stylesheet" type="text/css">';
 echo '</head><body>';
 
 navbar();
+
+$cfg = '../config.php';
+if (file_exists($cfg)) {
+    include($cfg);
+} else {
+    alert("<em>$cfg</em> file can't be found. You <b>must</b> create it.");
+    exit(1);
+}
+$dota = "$home/.steam/steam/steamapps/common/dota 2 beta/game/dota/";
+
+
 alert_exe($dota);
 file_perms($dota);
 $f=$dota;
@@ -87,8 +98,6 @@ for ($i=0; $i<6; $i++) {
     file_perms($f);
 }
 
-echo '<link href="style.css?3" rel="stylesheet" type="text/css">
-</head>';
 
 echo '<div id="chaps">';
 
