cc9839f1d0d3f991409bce6c59d3efeaf3712e7b
Jepasq <jepasq111@gmail.com>
2022-09-14 05:28:26 +0200

    Add explorer path to browser title if not empty

--- src/explorer.php
+++ src/explorer.php
@@ -4,10 +4,15 @@ include_once('page_layout.php');
 include_once('d2view.php');
 include_once('global.php');
 
-head("Explorer");
+$pwd = $_GET['pwd'];
+
+if (empty($pwd)) {
+    head("Explorer");
+} else {
+    head($pwd." - Explorer");
+}
 navbar("explorer");
 
-$pwd = $_GET['pwd'];
 
 echo "<p>Current working dir : $pwd</p>";
 echo "<p>Content :</p>";
