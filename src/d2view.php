a95fc9f76fdff8469e15a888d5760a5bfd9f1dfa
Jepasq <jepasq111@gmail.com>
2022-09-26 10:09:08 +0200

    Add Doxyfile file

--- src/d2view.php
+++ src/d2view.php
@@ -18,25 +18,34 @@ class D2view{
 
     }
 
+    /** Print files from the given node
+     *
+     *  \param $node The parent node array.
+     *  \param $pwd  The working dir used to print.
+     *
+     */
     function print_tree($node, $pwd=''){
         $hide='';
-        if (is_countable($node)) {
-            if(!is_null($node) && count($node) > 0) {
-                if(is_array($node)){
-                    foreach($node as $name=>$subn) {
-                        $fp = "$pwd/$name";
-                        // Only print top-level items
-                        echo "<a href='explorer.php?pwd=$fp'>$fp</a><br>";
-                        $this->nb_files++;
-                        //$print_tree($subn, $pwd);
-                        
+//        if (is_countable($node)) {
+        if(!is_null($node) && count($node) > 0) {
+            if(is_array($node)){
+                foreach($node as $name=>$subn) {
+                    $fp = "$pwd/$name";
+                    if ($pwd == "/") {
+                        $fp = $pwd.$name;
                     }
-                }else{ // Node
-                    echo "$node<br>";
-                    //                            echo $fp;
+                    // Only print top-level items
+                    echo "<a href='explorer.php?pwd=$fp'>$fp</a><br>";
+                    $this->nb_files++;
+                    //$print_tree($subn, $pwd);
+                    
                 }
+            }else{ // Node
+                echo "$node : $node->size bytes<br>";
+                //                            echo $fp;
             }
         }
+   //     }
     }
 
     /** List all files found in an archive directory
