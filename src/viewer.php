787632a8b339d7f95b5b9efc4e43d85870ce371d
Jepasq <jepasq111@gmail.com>
2022-10-17 11:56:57 +0200

    Add current file and extension to viewer

--- src/viewer.php
+++ src/viewer.php
@@ -6,19 +6,31 @@ include_once('page_layout.php');
 include_once('d2view.php');
 include_once('global.php');
 
-$path = $_GET['path'];
-$type = $_GET['type'];
+$file = $_GET['file'];
+$type = $_GET['type']; // Used to force type ?
 
-if (empty($path)) {
+
+function getFileType($filename) {
+    $path_parts = pathinfo($filename);
+    return $path_parts['extension'];
+}
+
+if (empty($file)) {
     head("Viewer");
 } else {
-    head($path." - Viewer");
+    head($file." - Viewer");
 }
 navbar("viewer");
 
-if (empty($path) && empty($type)) {
+if (empty($file) && empty($type)) {
     alert("This file viewer can be hard to use directly as a standalone
         feature. You may want to use it from the explorer instead.");
+} else {
+    echo "<section class='exporer-header'>";
+    echo "Currently viewing : $file <br>";
+    $type = getFileType($file);
+    echo "File type :  $type <br>";
+    echo "</section>";
 }
 
 
