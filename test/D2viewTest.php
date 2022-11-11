05d44a6899971feb790ff9013fee177800cbb0b8
Jepasq <jepasq111@gmail.com>
2022-11-11 14:21:51 +0100

    Add basic D2view::queryString unit test

--- test/D2viewTest.php
+++ test/D2viewTest.php
@@ -1,20 +1,26 @@
-
 <?php
 use PHPUnit\Framework\TestCase;
+
+// Please remenber that tests are called from root directory so, no need
+// to explicitely call in src/.
+include_once('src/d2view.php');
+require('config.php'); // Mainly for $home
+
+// We're not in the same directory than src/ sources. We can't just use
+// global.php
+$dota = "$home/.steam/steam/steamapps/common/dota 2 beta/game/dota/";
+
 
 class D2viewTest extends TestCase
 {
     public function testPushAndPop()
     {
-        $stack = [];
-        $this->assertSame(0, count($stack));
+        global $dota;
+        $d2 = new D2View($dota);
+        $list = $d2->queryString(".png");
 
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
+        $this->assertGreaterThan(count($list), 0);
+        // $this->assertSame(0, count($stack));
     }
 }
 ?>
