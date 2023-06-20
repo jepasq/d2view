9478e692eef06587fa63a5851edebacc9bb83cd5
Jepasq <jepasq111@gmail.com>
2023-06-20 07:46:05 +0200

    Correctly print admin page

--- src/admin/index.php
+++ src/admin/index.php
@@ -2,8 +2,13 @@
 
 $start_time = microtime(TRUE);
 
-include_once('page_layout.php');
-include_once('d2view.php');
-include_once('global.php');
+include_once('../page_layout.php');
+//include_once('../d2view.php');
+include_once('./global.php');
+
+head("Admin");
+navbar("admin");
+
+footer($start_time);
 
 ?>
