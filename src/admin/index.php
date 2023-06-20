f3b7e4fe4e9199c831ca3f9e045f9aa58482ddb3
Jepasq <jepasq111@gmail.com>
2023-06-20 08:12:59 +0200

    Add a sensitive data alert to the admin page

--- src/admin/index.php
+++ src/admin/index.php
@@ -9,6 +9,10 @@ include_once('./global.php');
 head("Admin");
 navbar("admin");
 
+alert("This page could show sensitive informations. <br>".
+"Please be sure you protected access to this page with a password.");
+
+
 footer($start_time);
 
 ?>
