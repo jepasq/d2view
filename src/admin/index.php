4ae3ea314741b5b15112578d7cc6c35433edd028
Jepasq <jepasq111@gmail.com>
2023-06-21 07:31:06 +0200

    Add show vars admin section

--- src/admin/index.php
+++ src/admin/index.php
@@ -2,9 +2,13 @@
 
 $start_time = microtime(TRUE);
 
+function show_value($var, $val) {
+    echo "$var : $val<br>";
+}
+
 include_once('../page_layout.php');
 //include_once('../d2view.php');
-include_once('./global.php');
+include('./global.php');
 
 head("Admin");
 navbar("admin");
@@ -12,6 +16,11 @@ navbar("admin");
 alert("This page could show sensitive informations. <br>".
 "Please be sure you protected access to this page with a password.");
 
+echo "<secion>";
+show_value("Home directory", $home);
+show_value("Dota directory", $dota);
+show_value("Extraction directory", $extractdir);
+echo "</secion>";
 
 footer($start_time);
 
