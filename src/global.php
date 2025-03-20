008b3cf0524fdc1bca1f8f5b369d5061343c4aad
Jepasq <jepasq111@gmail.com>
2025-03-20 09:25:10 +0100

    Try to include page_layout to error page to add styles

--- src/global.php
+++ src/global.php
@@ -4,8 +4,11 @@ $cfg = '../config.php';
 if (file_exists($cfg)) {
     include($cfg);
 } else {
+
+    include_once('page_layout.php'); /// To include styles
+    
     alert("<em>$cfg</em> file can't be found. You <b>must</b> create it.<br/>"
-    . "(Called from " . getcwd(). " )");
+    . "(Called from " . getcwd() . " )");
     exit(1);
 }
 $dota = "$home/.steam/steam/steamapps/common/dota 2 beta/game/dota/";
